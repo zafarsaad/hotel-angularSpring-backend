9a7c74b3115443f47a2616c259146a724a48b5a9
Saad Zafar <saadzafar@macbook-air.mynetworksettings.com>
2022-04-15 03:07:43 -0400

    GET, POST, PUT, DELETE Dummy APIs set

--- src/main/java/com/linkedinlearning/reactivespring/controller/ReservationResource.java
+++ src/main/java/com/linkedinlearning/reactivespring/controller/ReservationResource.java
@@ -1,5 +1,6 @@
 package com.linkedinlearning.reactivespring.controller;
 
+import com.linkedinlearning.reactivespring.model.Reservation;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -17,5 +18,26 @@ public class ReservationResource {
         //reservationService.getReservation(roomId);
 
         return Mono.just("{}");
+    }
+
+    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Mono<String> createReservation(@RequestBody Mono<Reservation> reservation) {
+
+        return Mono.just("{}");
+    }
+
+    @PutMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Mono<String> updatePrice(@PathVariable String roomId,
+            @RequestBody Mono<Reservation> reservation) {
+
+        return Mono.just("{}");
+    }
+
+    @DeleteMapping(path = "{roomId}")
+    public Mono<Boolean> deleteReservation(@PathVariable String roomId) {
+
+        return Mono.just(true);
     }
 }
